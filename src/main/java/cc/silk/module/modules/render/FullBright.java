30bc448e1fa38a5c50beb00da254a5c17bd10bf8
red213black-creator <red213black@gmail.com>
2026-08-18 13:31:13 +0300

    Update FullBright.java

--- src/main/java/cc/silk/module/modules/render/FullBright.java
+++ src/main/java/cc/silk/module/modules/render/FullBright.java
@@ -2,10 +2,15 @@ package cc.silk.module.modules.render;
 
 import cc.silk.module.Category;
 import cc.silk.module.Module;
+import cc.silk.module.setting.BooleanSetting;
 
 public class FullBright extends Module {
 
+    public static final BooleanSetting antiBlindness =
+            new BooleanSetting("Anti Blindness", false);
+
     public FullBright() {
         super("Full Bright", "Removes darkness", Category.RENDER);
+        addSetting(antiBlindness);
     }
 }
