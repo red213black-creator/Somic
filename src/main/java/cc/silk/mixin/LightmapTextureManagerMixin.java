d5a2781d6b07778fd7120cbf3f401374e4d3c199
red213black-creator <red213black@gmail.com>
2026-08-18 13:32:24 +0300

    Update LightmapTextureManagerMixin.java

--- src/main/java/cc/silk/mixin/LightmapTextureManagerMixin.java
+++ src/main/java/cc/silk/mixin/LightmapTextureManagerMixin.java
@@ -2,16 +2,30 @@ package cc.silk.mixin;
 
 import cc.silk.SilkClient;
 import cc.silk.module.modules.render.FullBright;
-import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LightmapTextureManager.class)
-public class LightmapTextureManagerMixin {
-    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
-    private static void setBrightness(CallbackInfoReturnable<Float> ci) {
-        if (SilkClient.INSTANCE.moduleManager.getModule(FullBright.class).get().isEnabled()) ci.setReturnValue(1.0F);
+@Mixin(WorldRenderer.class)
+public class BlindnessWorldRendererMixin {
+
+    @Inject(
+            method = "hasBlindnessOrDarkness",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void removeBlindness(Camera camera, CallbackInfoReturnable<Boolean> cir) {
+        var module = SilkClient.INSTANCE.moduleManager
+                .getModule(FullBright.class)
+                .orElse(null);
+
+        if (module != null
+                && module.isEnabled()
+                && FullBright.antiBlindness.getValue()) {
+            cir.setReturnValue(false);
+        }
     }
 }
